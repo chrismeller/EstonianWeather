7a169fc0d6a6d6e5041da418809592517c96c4c2
Chris Meller <chris@doesnthaveone.com>
2019-01-25 16:19:57 +0200

    Add a domain project with a starter service for Finnish forecasts.

---
+++ EstonianWeather.Domain/FinnishForecastService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading.Tasks;
+using Dapper;
+using EstonianWeather.Data;
+
+namespace EstonianWeather.Domain
+{
+    public class FinnishForecastService
+    {
+        private readonly DbConnection _db;
+
+        public FinnishForecastService(ApplicationDbContext dbContext)
+        {
+            _db = dbContext.GetDbConnection();
+        }
+    }
+}
